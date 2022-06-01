d386d04ad5ac8345cf5108972119f7722cf90121
lebinhthuan <104605059+lebinhthuan@users.noreply.github.com>
2022-06-01 23:10:40 +0700

    update - Buoc 2

--- spread-syntax-example.js
+++ spread-syntax-example.js
@@ -1,8 +1,7 @@
-
-// Before use spread operator
+// After use spread operator
 function sum(x, y, z) {
     return x + y + z;
 }
 
 const numbers = [1, 2, 3];
-console.log(sum.apply(null, numbers));
+console.log(sum(...numbers));
